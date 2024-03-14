2c01b422449f4d610848036bb3c92c577663b487
BENABBAS Amira <70579495+amira2001@users.noreply.github.com>
2024-03-14 14:49:34 +0100

    Update Part2M5

--- Part2M5.sql
+++ Part2M5.sql
@@ -1,5 +1,5 @@
-/*INSERT INTO authors (name, country) VALUES ('Amira', 'United Kingdom');
-*/
+INSERT INTO authors (name, country) VALUES ('Amira', 'United Kingdom');
+
 /*faire une jointure et traiter les valeurs monquante avec la fonction COALESCE*/
 SELECT A.* ,COALESCE(B.book_id, 'N/A') AS book_id
 FROM authors A
